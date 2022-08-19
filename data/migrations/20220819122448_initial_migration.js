486a1bd2ec5870dec8f90191230b41f5bdcca866
Mina Mikhael <mina.s.mikhael@gmail.com>
2022-08-19 14:58:49 +0200

    work in progress

---
+++ data/migrations/20220819122448_initial_migration.js
@@ -0,0 +1,53 @@
+exports.up = async function (knex) {
+  await knex.schema
+    .createTable("recipes", (tbl) => {
+      tbl.increments("recipe_id");
+      tbl.string("recipe_name", 200).notNullable().unique();
+    })
+    .createTable("ingredients", (tbl) => {
+      tbl.increments("ingredient_id");
+      tbl.string("ingredient_name", 100).notNullable().unique();
+      tbl.string("ingredient_unit", 100);
+    })
+    .createTable("steps", (tbl) => {
+      tbl.increments("step_id");
+      tbl.string("step_text", 250).notNullable();
+      tbl.integer("step_number", 100).notNullable().unsigned();
+      tbl
+        .integer("recipe_id")
+        .unsigned()
+        .notNullable()
+        .references("recipe_id")
+        .inTable("recipes")
+        .onUpdate("CASCADE")
+        .onDelete("CASCADE");
+    })
+    .createTable("step_ingredients", (tbl) => {
+      tbl.increments("step_ingredient_id");
+      tbl.float("quantity", 100).notNullable().unsigned();
+      tbl
+        .integer("step_id")
+        .unsigned()
+        .notNullable()
+        .references("step_id")
+        .inTable("steps")
+        .onUpdate("CASCADE")
+        .onDelete("CASCADE");
+      tbl
+        .integer("ingredient_id")
+        .unsigned()
+        .notNullable()
+        .references("ingredient_id")
+        .inTable("ingredients")
+        .onUpdate("CASCADE")
+        .onDelete("CASCADE");
+    });
+};
+
+exports.down = async function (knex) {
+  await knex.schema
+    .dropTableIfExists("step_ingredients")
+    .dropTableIfExists("steps")
+    .dropTableIfExists("ingredients")
+    .dropTableIfExists("recipes");
+};
--- knexfile.js
+++ knexfile.js
@@ -1,8 +1,28 @@
+const sharedConfig = {
+  client: "sqlite3",
+  useNullAsDefault: true,
+  migrations: { directory: "./data/migrations" },
+  pool: { afterCreate: (conn, done) => conn.run("PRAGMA foreign_keys= ON", done) },
+  seeds: { directory: "./data/seeds" },
+};
+
 module.exports = {
   development: {
-    client: "sqlite3",
+    ...sharedConfig,
     connection: {
-      filename: "./data.food.db3",
+      filename: "./data/food_recipes.db3",
+    },
+  },
+  testing: {
+    ...sharedConfig,
+    connection: {
+      filename: "./data/food_recipes.test.db3",
+    },
+  },
+  production: {
+    ...sharedConfig,
+    connection: {
+      filename: "./data/food_recipes.prod.db3",
     },
   },
 };
